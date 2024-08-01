447cac20f8f68bf3cc63e67bf7b93aa1ee26fc95
Devlin Lynch <devklynch@gmail.com>
2024-08-01 17:53:10 -0600

    Add built_in_methods responses

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,28 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+#The include question mark method is being called on the string "Hello World" with the argument of "Hello"
+#The method is checking if the argument "Hello" is in the string "Hello World" and returning the Boolean response
+#The return value is true
 "Hello World".include?("Hello")
 
-
+#The end_with question mark method is being called on the string "Hello World" with the argument of "Hello"
+#The method is checking if the argument "Hello" is at the end of the string "Hello World"
+#The return value is false
 "Hello World".end_with?("Hello")
 
-
+#The end_with question mark method is being called on the string "Hello World" with the argument of "rld"
+#The method is checking if the argument "rld" is at the end of the string "Hello World"
+#The return value is true
 "Hello World".end_with?("rld")
 
-
+#The even question mark method is being called on the integer 12
+#12 is an even number so the response is true
 12.even?
 
-
+#The next method is called on the integer 18
+#It responds with the next integer after 18
+#The response is 19
 18.next
 
 
@@ -40,12 +49,35 @@ first_name = "Jeff"
 puts first_name.start_with?("J")
 
 
+#The reverse method is being called on the variable string "name", which is "Devlin"
+#The reverse method returns the string in reverse, where the last character now becomes the first and so on.
+#In this case it would return "nilveD"
+name="Devlin"
+name.reverse
+
+#The chop method is being called on the variable string "color", which is "yellow"
+#The chop method returns the string without the last character
+#In this case it would return "yello"
+color="yellow"
+color.chop
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+#The to_f method is being called on the variable integer "human_age", which is 33
+#the to_f method returns the integer as a float
+#It returns 33.0
+human_age=33
+human_age.to_f
+
+#The zero? method is being called on the variable integer "dog_age", which is 9
+#The zero? method is a Boolean and returns true if the value is zero
+#It would return false since 9 is not 0
+dog_age=9
+dog_age.zero?
 
 
 
@@ -54,3 +86,17 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+
+#The max method is being called on the integer array "array_test"
+#With no additional argument and block, it will return the element with the largest value
+#It would return 8
+array_test=[2,4,6,8]
+array_test.max
+
+
+#The sort method is being called on the string array "students"
+#The sort method with return with the elements in the array in alphabetical order (or numerical when it's an integer)
+#This would return ["Devlin","Matt","Rupert"]
+students=["Matt","Devlin","Rupert"]
+students.sort
